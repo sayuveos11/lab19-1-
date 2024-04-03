bc1221f385d9915e1e0bf0e5bed270ddacd7742f
sayuveos11 <kkallinberg@gmail.com>
2024-04-03 21:54:53 +0300

    Добавьте файлы проекта.

---
+++ lab19 (1)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        int k = 1234; // трьохзначне число
+
+        // Отримання рядка з числа
+        string numberString = k.ToString();
+
+        // Перевірка, чи число має три цифри
+        if (numberString.Length == 3)
+        {
+            // Отримання лівої, середньої та правої цифри
+            char C2 = numberString[0];
+            char C1 = numberString[1];
+            char C0 = numberString[2];
+
+            // Виведення результатів
+            Console.WriteLine($"Ліва цифра: {C2}");
+            Console.WriteLine($"Середня цифра: {C1}");
+            Console.WriteLine($"Права цифра: {C0}");
+        }
+        else
+        {
+            Console.WriteLine("Введіть трьохзначне число.");
+        }
+    }
+}
